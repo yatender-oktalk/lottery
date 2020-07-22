151843aee90da949f88478b94924ddf90be71af7
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-22 12:56:46 +0530

    added test for checking the winner

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -74,4 +74,32 @@ describe('Lottery Contract', () => {
       assert(error);
     }
   });
+
+  it('only manager can call pick winner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      })
+      assert(false);
+    } catch (error) {
+      assert(error);
+    }
+  });
+
+  it('sends money to winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("2", "ether")
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    console.log(lottery)
+    await lottery.methods.pickWinner().
+      send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei('1.8', "ether"));
+  })
 });
